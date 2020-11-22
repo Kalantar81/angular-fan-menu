e87452f3c93247bc4824fdeab64771c2aed35576
Peter Pshenichny <peter.pshenichny@gmail.com>
2020-11-22 21:51:07 +0200

    private methods and properties will cause the troubles when AOT, removed unnecessary setters

--- src/app/fan-gooey-menu/ngx-fan-gooey-menu/ngx-fan-gooey-menu.component.ts
+++ src/app/fan-gooey-menu/ngx-fan-gooey-menu/ngx-fan-gooey-menu.component.ts
@@ -2,63 +2,27 @@ import {
   Component,
   EventEmitter,
   Input,
-  OnInit,
   Output,
 } from '@angular/core';
 import { IFanGooeyMenuButton } from '../models/IFan-menu-interfaces';
-import { EnumMenuLocation } from '../models/menu-enums';
 
 @Component({
   selector: 'ngx-fan-gooey-menu',
   templateUrl: './ngx-fan-gooey-menu.component.html',
   styleUrls: ['./ngx-fan-gooey-menu.component.css'],
 })
-export class NgxFanGooeyMenuComponent implements OnInit {
-
-  @Input()
-  set isOpenAtStart(p_isOpenAtStart: boolean) {
-    this._isOpenAtStart = p_isOpenAtStart;
-  }
-
-  @Input()
-  set fanMenuBtnColor(p_fanMenuBtnColor: string) {
-    this._fanMenuBtnColor = p_fanMenuBtnColor;
-  }
-
-  @Input()
-  set menuIconName(p_menuIconName: string) {
-    this._menuIconName = p_menuIconName;
-  }
-
-  @Input()
-  set menuSize(p_menuSize: number) {
-    this._menuSize = p_menuSize;
-  }
-
-  @Input()
-  set gooeyItems(p_gooeyItems: Array<IFanGooeyMenuButton>) {
-    this._gooeyItems = p_gooeyItems;
-  }
+export class NgxFanGooeyMenuComponent {
+  @Input() isOpenAtStart: boolean;
+  @Input() fanMenuBtnColor: string;
+  @Input() menuIconName: string;
+  @Input() menuSize: number;
+  @Input() gooeyItems: Array<IFanGooeyMenuButton> = [];
 
   @Output()
   combineMenuActionName: EventEmitter<string> = new EventEmitter<string>();
 
-  private _isOpenAtStart: boolean;
-  private _fanMenuBtnColor: string;
-  private _menuIconName: string;
-  private _menuSize: number;
-  private _gooeyItems: Array<IFanGooeyMenuButton> = [];
-  private _menuLocation: EnumMenuLocation;
-
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
-
-  private _getcombineMenuActionName(p_activateAction: string): void {
-    this.combineMenuActionName.emit(p_activateAction);
+  public getCombineMenuActionName(activateAction: string): void {
+    this.combineMenuActionName.emit(activateAction);
   }
 
 }
